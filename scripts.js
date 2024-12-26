970bc6cd2b8aa9d4950cb41d2d2bd1fc23a3ba04
lisiswebsite <lisiswebsite@gmail.com>
2024-12-26 16:24:42 -0500

    changed scripts.js back to original

--- scripts.js
+++ scripts.js
@@ -23,33 +23,8 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     if (!valid) {
         alert(errorMessages.join('\n'));
     } else {
-        const formData = new FormData(document.getElementById('quizForm'));
-        const data = {};
-        formData.forEach((value, key) => {
-            if (!data[key]) {
-                data[key] = [];
-            }
-            data[key].push(value);
-        });
-
-        fetch('https://script.google.com/macros/s/AKfycbwla3SwrI3hFDpApFbqHyGwNfV2P6SiHqKViWhalwZiEjlpUCDcylYRE3tse7N3n1Va/exec', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(result => {
-            if (result.result === 'success') {
-                alert('Form submitted successfully!');
-            } else {
-                alert('There was an error submitting the form.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('There was an error submitting the form.');
-        });
+        alert('Form submitted successfully!');
+        // Submit the form
+        document.getElementById('quizForm').submit();
     }
 });
