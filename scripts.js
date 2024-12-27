dd3a01bf8e43b91877b2fe92e2f1a770b45a393d
lisiswebsite <lisiswebsite@gmail.com>
2024-12-26 22:22:20 -0500

    wording updates

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ const form = document.forms['vote-form']
 
 // Define the required number of options for each position
 const requiredSelections = {
-  position1: 3, // Users must select 3 options for position 1
-  position2: 1,
-  position3: 8,
-  position4: 2,
-  position5: 6,
-  position6: 5,
-  position7: 9
+  "catchers in section 1": 3, // Users must select 3 options for catcher (Question 1)
+  "first baseman in section 2": 1,
+  "infielders in section 3": 8,
+  "utility players in section 4": 2,
+  "outfielders in section 5": 6,
+  "left-handed pitchers in section 6": 5,
+  "right-handed pitchers in section 7": 9
 };
 
 form.addEventListener('submit', e => {
@@ -21,7 +21,7 @@ form.addEventListener('submit', e => {
   for (const [position, required] of Object.entries(requiredSelections)) {
     const selectedOptions = document.querySelectorAll(`input[data-position="${position}"]:checked`);
     if (selectedOptions.length !== required) {
-      alert(`Please select exactly ${required} option(s) for ${position}.`);
+      alert(`Please select exactly ${required} ${position}.`);
       allValid = false;
       break;
     }
@@ -30,7 +30,7 @@ form.addEventListener('submit', e => {
   if (allValid) {
     // Submit the form if all validations pass
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then((response) => alert("Thank you! Form is submitted"))
+      .then((response) => alert("Your ballot has been cast. Thanks for participating!"))
       .then(() => { window.location.reload(); })
       .catch((error) => console.error('Error!', error.message));
   }
